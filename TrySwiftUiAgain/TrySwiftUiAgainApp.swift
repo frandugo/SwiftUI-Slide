6f4d5344dd22bc6daccbd9cee23f58211b9332d3
frandugo <fduran@merqueo.com>
2022-04-28 16:01:39 -0500

    Initial Commit

---
+++ TrySwiftUiAgain/TrySwiftUiAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySwiftUiAgainApp.swift
+//  TrySwiftUiAgain
+//
+//  Created by Merqueo on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TrySwiftUiAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
