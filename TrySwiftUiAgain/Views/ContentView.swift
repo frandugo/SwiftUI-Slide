ee972f60da0318ed7988fe3544a636bea898c21b
frandugo <fduran@merqueo.com>
2022-05-13 09:33:50 -0500

    Adding new features to the app - popup with slider value

--- TrySwiftUiAgain/Views/ContentView.swift
+++ TrySwiftUiAgain/Views/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isAlertVisible: Bool = false
+    @State private var whosThereIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    
+    @State private var game: Game = Game()
+    
     var body: some View {
         VStack {
             HStack(){
@@ -19,22 +26,30 @@ struct ContentView: View {
                         .lineSpacing(4.0)
                         .font(.footnote)
                         .padding(.bottom, 10.0)
-                    Text("89")
+                    Text(String(game.target))
                         .kerning(-1.0)
-                    
                         .font(.system(size: 36, weight: .black))
                     HStack(){
                         Text("1")
                             .font(.system(size: 18, weight: .bold))
-                        Slider(value: .constant(50), in: 1.0...100.0)
+                        Slider(value: self.$sliderValue, in: 1.0...100.0)
                         Text("100")
                             .font(.system(size: 18, weight: .bold))
                     }
                     Button(action: {
-                        print("Hey Hello SwiftUI")
+                        self.isAlertVisible = true
                     }) {
-                        /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        Text("Hit me!!!")
                     }
+                    .alert(
+                        isPresented: $isAlertVisible,
+                        content: {
+                            var roundedValue: Int = Int(self.sliderValue.rounded())
+                            return Alert(
+                                title: Text("Hello there!!!"),
+                                message: Text("The current value is  \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round."),
+                                dismissButton: .default(Text("Awesome!!!")))
+                    })
                 }
             }
         }
--- TrySwiftUiAgainTests/TrySwiftUiAgainTests.swift
+++ TrySwiftUiAgainTests/TrySwiftUiAgainTests.swift
@@ -9,25 +9,28 @@ import XCTest
 @testable import TrySwiftUiAgain
 
 class TrySwiftUiAgainTests: XCTestCase {
+    
+    var game: Game!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        game = Game()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testScorePositive(){
+        var guess = game.target + 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score,95)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testScoreNegative(){
+        var guess = game.target - 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
     }
-
 }
